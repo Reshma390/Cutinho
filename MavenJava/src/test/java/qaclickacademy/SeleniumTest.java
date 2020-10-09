32fa81c786f8b46e144766464a5f6d277eab70f2
ReshmaCutinho <cutinho.rish@gmail.com>
2020-10-09 15:03:10 +0100

    first commit

---
+++ MavenJava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	@Test
+	public void browserTest()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void loginTest()
+	{
+		System.out.println("deleteTwitter");	
+	}
+}
